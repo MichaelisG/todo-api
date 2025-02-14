70288745ac514d75a03b1d892fab0c57704ce8b9
MichaelisG <gregoriades.michaelis@gmail.com>
2025-02-14 08:12:31 +0100

    3- Expose Entites via the API

--- src/Repository/TodoCategoryRepository.php
+++ src/Repository/TodoCategoryRepository.php
@@ -15,29 +15,4 @@ class TodoCategoryRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, TodoCategory::class);
     }
-
-    //    /**
-    //     * @return TodoCategory[] Returns an array of TodoCategory objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?TodoCategory
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
